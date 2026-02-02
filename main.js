526af0ec35d7b8cf55fa279b129227583718729a
cda79 <stintdang@gmail.com>
2026-02-02 00:35:38 -0800

    cleaning

---
+++ main.js
@@ -0,0 +1,41 @@
+// alert("HELLO")
+// console.log("HELLO CONSOLE")
+
+// let introContainer = document.querySelectorAll(".intro-container");
+// console.log(introContainer);
+
+// introContainer.forEach((text)=>{
+//     text.style.color = "blue";
+// });
+
+// introContainer.forEach((text)=>{
+//     text.addEventListener("click",()=>{
+//     alert("HELLO CLICKED");
+//     text.style.color = "blue";
+//     })
+// });
+
+// //container
+// // TODO create a function that takes x y width height and fill 
+// // and generates a rectangle inside of the svg
+// let svg = document.querySelector("svg");
+// console.log(svg);
+
+// //element
+// const svgNS = "http://www.w3.org/2000/svg"
+// const circle = document.createElementNS(svgNS, "circle");
+
+// circle.setAttributeNS(null,"r",45)
+// circle.setAttributeNS(null,"cx",100);
+// circle.setAttributeNS(null,"cy",100);
+// circle.setAttributeNS(null,"fill","blue");
+// circle.setAttributeNS(null,"stroke","red");
+// circle.setAttributeNS(null,"stroke-width","3");
+// //add element into container
+// svg.appendChild(circle);
+
+// for html:
+        // <svg width="800" height="800">
+        //     <circle r="45" cx="50" cy="50" fill="red" stroke="green" stroke-width="3" />
+        //     <circle r="45" cx="150" cy="150" fill="red" stroke="green" stroke-width="3" />
+        // </svg>
