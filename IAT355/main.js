73b186da6e5804f5eb89868f129085def66ca501
cda79 <138259578+cda79@users.noreply.github.com>
2026-04-03 23:30:34 -0700

    basic layout

--- IAT355/main.js
+++ IAT355/main.js
@@ -22,10 +22,75 @@ fetchData().then(async (data) => {
       ]),
     )
     .width("container")
+    .height(500)
+    .toSpec();
+
+  const showtypes = [
+    ...new Set(
+      data
+        .map((d) => d.show_type)
+        .filter((val) => val !== null && val !== undefined),
+    ),
+  ];
+
+  const selectType = vl
+    .selectPoint("Select")
+    .fields("show_type")
+    .bind(
+      vl
+        .menu([null, ...showtypes])
+        .labels(["All", ...showtypes])
+        .name("Choose Show Type: "),
+    );
+
+  const vlSpec2 = vl
+    .markCircle()
+    .data(data)
+    .params(selectType)
+    .encode(
+      vl.x().fieldT("opening_date").timeUnit("year").title("Year Opened"),
+      vl.y().count().title("Number of Titles Opened"),
+      vl.color().fieldN("show_type").title("Show Type"),
+      vl.opacity().if(selectType, vl.value(0.75)).value(0.05),
+      vl.tooltip([
+        { field: "opening_date", timeUnit: "year", title: "Year" },
+        { field: "show_type", title: "Show Type" },
+        { aggregate: "count", title: "Count" },
+      ]),
+    )
+    .width("container")
+    .height(500)
+    .toSpec();
+
+  const musicalData = data.filter((d) => d.show_type === "Musical");
+
+  const vlSpec3 = vl
+    .markBar()
+    .data(musicalData)
+    .transform(
+      // 1. Extract the year, 2. Calculate the decade
+      vl.calculate("year(datum.opening_date)").as("year_only"),
+      vl.calculate("floor(datum.year_only / 10) * 10").as("decade"),
+    )
+    .encode(
+      vl.x().fieldO("decade").title("Decade"),
+      vl.y().count().title("Number of Shows"),
+      vl.color().fieldN("production_type").title("Production Type"),
+      vl.tooltip([
+        { field: "decade", title: "Decade" },
+        { field: "production_type", title: "Production Type" },
+        { aggregate: "count", title: "Count" },
+      ]),
+    )
+    .width("container")
+    .height(300)
+    .title("Original vs Revival Plays on Broadway by Decade")
     .toSpec();
 
   // Render the graphs
   render("#P1-1", vlSpec);
+  render("#P2-1", vlSpec2);
+  render("#P3-1", vlSpec3);
 });
 
 async function render(viewID, spec) {
